e8499c0216911c2defe20649e8c4af512b7272c8
Eoghan Perkins <perkinseoghan@gmail.com>
2024-07-01 21:49:54 -0600

    Add basic SQLite3 database interaction
    Commands include "!addmovie, !cq,
    !upvote, and !moviebot."
    
    Commands are barebones, unfrefined, and not yet
    ready for deployment.

--- bin/dart_discord_bot.dart
+++ bin/dart_discord_bot.dart
@@ -1,7 +1,6 @@
 import 'package:nyxx/nyxx.dart';
 import 'dart:io';
-import 'dart:async';
-import 'package:sqlite3/sqlite3.dart';
+import '../dbcode.dart';
 
 
 void main() async {
@@ -19,6 +18,9 @@ void main() async {
     print('Error reading API token config file: $e');
     return;
   }
+
+  // Initialize the database
+  initDatabase();
   
   // Access the Discord API
   final bot = NyxxFactory.createNyxxWebsocket(token, GatewayIntents.allUnprivileged)
@@ -28,45 +30,60 @@ void main() async {
 
   print("Bot Running");
   
-  // 
+  // Print Hello message to channel upon bot arrival
   bot.eventsWs.onGuildCreate.listen((event) {
     for (var channel in event.guild.channels) {
       if (channel is ITextGuildChannel) {
-        channel.sendMessage(MessageBuilder.content('Hello! Your movie guru has arrived!'));
+        channel.sendMessage(MessageBuilder.content('Hello! Your movie guru has arrived! Respond with "!moviebot" to learn more.'));
         break; // Send only to the first text channel found
       }
     }
   });
 
-  bot.eventsWs.onMessageReceived.listen((event) {
-    if (event.message.content.contains('!moviebot') ) {
-      event.message.channel.sendMessage(MessageBuilder.content('Hello! I am Discord bot written using Dart programming, how can I help you?'));
-      print("Responding to hello");
+  // RESPOND TO BOT MESSAGES
+  
+  bot.eventsWs.onMessageReceived.listen((event) async {
+    final channelId = event.message.channel.id.toString();
+    
+    if (event.message.content.startsWith('!moviebot') ) {
+      try {
+        final greeting = await File('greeting.txt').readAsLines();
+        
+        event.message.channel.sendMessage(MessageBuilder.content(greeting.join('\n')));
+      } catch(e) {
+        event.message.channel.sendMessage(MessageBuilder.content('Greeting file failed to load'));
+        print(e);
+      }
     }
+    
+    if (event.message.content.startsWith('!addmovie')) {
+        final args = event.message.content.split(' ');
+        final title = args.sublist(1, args.length-1).join(' ');
+        final userId = event.message.author.id.toString();
+
+        print('Adding film to queue');
+        addMovie(userId, channelId, title, 1);
+        event.message.channel.sendMessage(MessageBuilder.content('Movie added to queue!'));
+    }
+    
+    if (event.message.content.startsWith('!cq')) {
+      final films = getFilms(channelId);
+      final result = films.map((film) => '${film['film_title']} (Rank: ${film['rank']})').join('\n');
+      
+      print('Printing film queue');
+      event.message.channel.sendMessage(MessageBuilder.content(result.isEmpty ? 'No Movies Found.' : result));
+    }
+
+    if (event.message.content.startsWith('!upvote')) {
+      final args = event.message.content.split(' ');
+      final film = args.sublist(1).join(' ');
+      
+      print('Upvoting $film');
+      upvote(channelId, film);
+      event.message.channel.sendMessage(MessageBuilder.content('$film upvoted'));
+    }
+
   });
-
-  // INITIALIZE DATABASE FOR MOVIES AND CREATE COMMANDS
-
-  void init_database() {
-    final db = sqlite3.open('movie_list.db');
-
-    db.execute (''' 
-      CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      user_id TEXT NOT NULL);    
-    ''');
-
-    db.execute ('''
-    CREATE TABLE IF NOT EXISTS movies (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      user_id TEXT NOT NULL,
-      movie_name TEXT NOT NULL,
-      priority INTEGER NOT NULL);
-  ''');
-  }
-
- 
-
+  
   bot.connect();
 }
-
---
+++ dbcode.dart
@@ -0,0 +1,66 @@
+import 'package:sqlite3/sqlite3.dart';
+
+void initDatabase() {
+    final db = sqlite3.open('movies.db');
+
+    db.execute (''' 
+      CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      user_id TEXT NOT NULL);    
+    ''');
+
+    db.execute ('''
+    CREATE TABLE IF NOT EXISTS movies (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      user_id TEXT NOT NULL,
+      channel_id TEXT NOT NULL,
+      film_title TEXT NOT NULL,
+      rank INTEGER NOT NULL);
+    '''); 
+
+  db.dispose();
+
+  }
+
+void addMovie(String userID, String channelId, String title, int rank){
+  final db = sqlite3.open("movies.db");
+
+  db.execute('''
+    INSERT INTO movies (user_id, channel_id, film_title, rank)
+    VALUES (?, ?, ?, ?)
+    ''', [userID, channelId, title, rank]);
+
+  
+  db.dispose();
+}
+
+List<Map <String, dynamic>> getFilms(String channelId) {
+  final db = sqlite3.open('movies.db');
+
+  final result = db.select('''
+    SELECT film_title, rank
+    FROM movies
+    WHERE channel_id = ?
+    ORDER BY rank DESC
+  ''', [channelId]);
+
+  final films = result.map((row) => {
+    'film_title': row['film_title'],
+    'rank': row['rank']
+  }).toList();
+
+  db.dispose();
+  return films;
+}
+
+void upvote(String channelId, String title) {
+  final db =sqlite3.open('movies.db');
+
+  db.execute('''
+    UPDATE movies
+    SET rank = rank + 1
+    WHERE channel_id = ? AND film_title = ?
+  ''', [channelId, title]);
+
+  db.dispose();
+}
